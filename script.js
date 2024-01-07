5057d10216125157f1b755113234c745db69dd57
Rachael Potter <rachaelpotter32@gmail.com>
2024-01-07 20:32:01 +0000

    refined javascript and added get date function

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const apiUrlToday = "https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=d4fbb78046f8d1022d9982c8b231ed15&units=metric&q=";
-const apiUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=d4fbb78046f8d1022d9982c8b231ed15&units=metric&q=";
+const apiUrlForecast = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&exclude=hourly&appid=d4fbb78046f8d1022d9982c8b231ed15&units=metric&q=";
 
-const searchBar = document.querySelector(".form input");
-const searchBtn = document.querySelector(".form button");
+const searchBar = document.querySelector(".search-section input");
+const searchBtn = document.querySelector(".search-section button");
 const weatherIcon = document.querySelector(".weather-icon");
+const forecasttemp = document.querySelector('temp-info');
+const todaysDate = document.querySelector(".today");
 
+todaysDate.innerHTML = new Date().toLocaleDateString("en-UK");
 
 async function getWeather(city) {
     const response = await fetch(apiUrlToday + city);
     let data = await response.json()
-    console.log(data) //checking the function correctly processes the api call
- 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
@@ -27,24 +28,24 @@ async function getWeather(city) {
     else if(data.weather[0].main === "storm") {
         weatherIcon.src = "/images/storm.jpg";
     }
-
+  console.log(data)
 
 }
 
-async function getForecast(city) {
-    const request = await fetch(apiUrlForecast + city);
-   .then(response => response.json())
-   .then(data => {
-    for(i=0;i<5;i++){
-        document.querySelector".day-forecast".innerHTML(i+1);
-    }
-   })
-    console.log(forecast)
+ function getForecast(city) {
+    fetch(apiUrlForecast + city)
+    .then(response => response.json())
+    .then(data =>{
+        for(i=0;i<5;i++){
+            const dayData = data.list[i];
+            document.querySelector(".day"+i).innerHTML= 'temp ' + Math.round(Number(dayData.main.temp)) + "°C";
+        }
+    })
 }
 
 searchBtn.addEventListener("click", ()=>{
     getWeather(searchBar.value); //calling function on mouse click of searchbox
-    getForecast(searchBar.value);
+    getForecast(searchBar.value)
     })
 
 
